26245a4622a2b1128eb82604d7e624f319526ab0
JhonM <jhon@example.com>
2023-04-20 15:14:45 -0500

    Agregar archivos de proyecto.

---
+++ Controlador/bd.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario_Cliente05.Controlador
+{
+
+    //Crecion de la base de datos
+    public class bd
+    {
+
+    }
+}
